84b0abc262b78a00e34e91a7309c8af99f44eb57
JOdiemo <odiemo@gmail.com>
2023-07-13 17:47:58 +0300

    added components

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,19 +1,32 @@
-/* eslint-disable */
+import PropTypes from 'prop-types';
 import calculate from './logic/calculate';
-const Button = ({ obj, setObj, value, classList }) => {
+
+const Button = ({
+  obj, setObj, value, classList,
+
+}) => {
   const handleInput = (e) => {
     setObj(calculate(obj, e.target.innerText));
   };
 
   return (
     <button
+      type="button"
       className={classList}
       onClick={(e) => {
         handleInput(e);
-      }}>
+      }}
+    >
       {value}
     </button>
   );
+};
+
+Button.propTypes = {
+  obj: PropTypes.string.isRequired,
+  setObj: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  classList: PropTypes.string.isRequired,
 };
 
 export default Button;
