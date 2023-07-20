f20693b98ec1caa9e23fdb94489b4dc6da3f1fe3
JOdiemo <odiemo@gmail.com>
2023-07-20 22:02:47 +0300

    Fix linter Errors

--- src/__tests__/UserInt.test.js
+++ src/__tests__/UserInt.test.js
@@ -2,15 +2,18 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
 import App from '../App';
 
 describe('Testing user interactions on the main component (App.js).', () => {
   describe('When user clicks on Home button', () => {
-    it('should render the home page', () => {
-      render(<App />);
-      userEvent.click(screen.getByRole('link', { name: /home/i }));
+    it('should render the home page', async () => {
+      act(() => {
+        render(<App />);
+      });
+      await userEvent.click(screen.getByRole('link', { name: /Home/i }));
       expect(
-        screen.getByRole('heading', { name: /welcome to our page/i })
+        screen.getByRole('heading', { name: /welcome to our page/i }),
       ).toBeInTheDocument();
     });
   });
